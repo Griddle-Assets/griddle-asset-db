c6cf4f2a6c72dee5471a33e348f30aac697aaa83
Thomas Shaw <printer.83mph@gmail.com>
2024-03-22 21:51:31 -0400

    Setup tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/renderer/index.html', './src/renderer/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+    },
+  },
+  plugins: [],
+};
