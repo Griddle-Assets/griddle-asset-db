1a8ed6257a65658e42badca1a3b7549b64336b5f
helenazzzzz <helenazhang803@gmail.com>
2024-03-25 17:30:53 -0400

    metadata

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -3,17 +3,11 @@ import fetchClient from './lib/fetch-client';
 import Metadata from './components/Metadata';
 
 import { useState } from 'react';
-
-interface Asset {
-  asset_name: string;
-  keywords: string;
-  image_url: string | null;
-  id: string;
-  author_pennkey: string;
-}
+import { Asset } from './types';
 
 function App(): JSX.Element {
   const [assets, setAssets] = useState<Asset[]>([]);
+  const [selectedAsset, setSelectedAsset] = useState<Asset | null>(null);
 
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
   const fetchAssets = async () => {
@@ -25,23 +19,12 @@ function App(): JSX.Element {
     }
   };
 
+  const handleAssetClick = (asset: Asset) => {
+    setSelectedAsset(asset);
+  };
+
   return (
     <>
-      <div className="my-6 text-4xl font-bold">Our app here!</div>
-      <button
-        type="button"
-        onClick={ipcHandle}
-        className="px-6 py-2 bg-blue-500 text-white font-medium rounded-full mb-4 hover:bg-blue-600"
-      >
-        Send IPC
-      </button>
-      <button
-        type="button"
-        onClick={fetchAssets}
-        className="px-6 py-2 bg-blue-500 text-white font-medium rounded-full mb-4 hover:bg-blue-600"
-      >
-        Fetch assets
-      </button>
       <div className="my-6 text-4xl font-bold">Griddle</div>
       <div className="flex">
         <div className="w-2/3">
@@ -52,16 +35,31 @@ function App(): JSX.Element {
           >
             Send IPC
           </button>
+          <button
+            type="button"
+            onClick={fetchAssets}
+            className="px-6 py-2 bg-blue-500 text-white font-medium rounded-full mb-4 hover:bg-blue-600"
+          >
+            Fetch assets
+          </button>
           <Versions></Versions>
           <ul>
             {assets.map((asset, index) => (
               <li key={index}>
-                <div>{asset.asset_name}</div>
+                <button
+                  type="button"
+                  className="px-6 py-2 bg-blue-500 text-white font-medium rounded-full mb-4 hover:bg-blue-600"
+                  onClick={() => handleAssetClick(asset)} // Call handleAssetClick with the selected asset
+                >
+                  {asset.asset_name}
+                </button>
               </li>
             ))}
           </ul>
         </div>
-        <Metadata></Metadata>
+        <div className="w-1/4">
+          <Metadata asset={selectedAsset} />
+        </div>
       </div>
     </>
   );
