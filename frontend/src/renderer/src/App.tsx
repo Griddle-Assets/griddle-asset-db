1db2176e6a3558cb90ac9c12d4634a6fbcf673d2
helenazzzzz <helenazhang803@gmail.com>
2024-03-24 22:32:52 -0400

    Metadata panel and formatting

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -1,19 +1,26 @@
 import Versions from './components/Versions';
+import Metadata from './components/Metadata';
 
 function App(): JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
 
   return (
     <>
-      <div className="my-6 text-4xl font-bold">Our app here!</div>
-      <button
-        type="button"
-        onClick={ipcHandle}
-        className="px-6 py-2 bg-blue-500 text-white font-medium rounded-full mb-4 hover:bg-blue-600"
-      >
-        Send IPC
-      </button>
-      <Versions></Versions>
+    <div className="my-6 text-4xl font-bold">Griddle</div>
+    <div className='flex'>
+      <div className="w-2/3">
+        <button
+          type="button"
+          onClick={ipcHandle}
+          className="px-6 py-2 bg-blue-500 text-white font-medium rounded-full mb-4 hover:bg-blue-600"
+        >
+          Send IPC
+        </button>
+        <Versions></Versions>
+        </div>
+      <Metadata></Metadata>
+    </div>
+      
     </>
   );
 }
---
+++ frontend/src/renderer/src/components/Metadata.tsx
@@ -0,0 +1,9 @@
+function Metadata(): JSX.Element {
+    return(
+        <div className="flex-1 w-1/4">
+            <div className="text-lg">Metadata</div>
+        </div>
+    );
+}
+
+export default Metadata;
