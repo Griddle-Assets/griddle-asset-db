dabda30341fa4d6c9dc05bc1f97967688b391201
helenazzzzz <helenazhang803@gmail.com>
2024-03-25 17:06:39 -0400

    fetching assets

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -2,14 +2,26 @@ import Versions from './components/Versions';
 import fetchClient from './lib/fetch-client';
 import Metadata from './components/Metadata';
 
+import { useState } from 'react';
+
+interface Asset {
+  asset_name: string;
+  keywords: string;
+  image_url: string | null;
+  id: string;
+  author_pennkey: string;
+}
+
 function App(): JSX.Element {
+  const [assets, setAssets] = useState<Asset[]>([]);
+
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
   const fetchAssets = async () => {
     const { data, error } = await fetchClient.GET('/api/v1/assets/');
-    if (error) {
-      console.error(error);
+    if (!data) {
+      throw error;
     } else {
-      alert(data.map(({ asset_name }) => asset_name).join(', '));
+      setAssets(data);
     }
   };
 
@@ -30,7 +42,6 @@ function App(): JSX.Element {
       >
         Fetch assets
       </button>
-      <Versions></Versions>
       <div className="my-6 text-4xl font-bold">Griddle</div>
       <div className="flex">
         <div className="w-2/3">
@@ -42,6 +53,13 @@ function App(): JSX.Element {
             Send IPC
           </button>
           <Versions></Versions>
+          <ul>
+            {assets.map((asset, index) => (
+              <li key={index}>
+                <div>{asset.asset_name}</div>
+              </li>
+            ))}
+          </ul>
         </div>
         <Metadata></Metadata>
       </div>
