1d94da1c1ea72f7a5a9477adbe4d16eebf10b492
Thomas Shaw <printer.83mph@gmail.com>
2024-03-27 23:18:57 -0400

    Update homepage styling for responsiveness and scroll

--- frontend/src/renderer/src/routes/home-view.tsx
+++ frontend/src/renderer/src/routes/home-view.tsx
@@ -20,25 +20,27 @@ function HomeView(): JSX.Element {
 
   return (
     <>
-      <div className="w-screen h-screen absolute inset-0 min-w-[800px] overflow-clip">
+      <div className="w-screen h-screen max-h-screen min-w-[400px] overflow-clip grid grid-rows-[min-content_1fr]">
         <Navbar />
-        <div className="grid flex-grow grid-cols-[minmax(min-content,calc(min(25%,320px)))_minmax(min-content,1fr)_minmax(min-content,calc(min(25%,320px)))]">
-          <div className="border-r-[1px] border-base-content/20 py-4 px-6">
+        <div className="grid grid-cols-[minmax(160px,calc(min(25%,320px)))_minmax(0,1fr)_minmax(160px,calc(min(25%,320px)))]">
+          <div className="relative border-r-[1px] border-base-content/20">
             {/* Asset explorer */}
-            <p className="text-base-content/60">Explorer</p>
+            <div className="absolute inset-0 py-4 px-6">
+              <p className="text-base-content/60">Explorer</p>
+            </div>
           </div>
           {/* Main body */}
           <div
             onClick={() => {
               setSelectedAssetId(null);
             }}
-            className="py-4 px-6 bg-base-200 overflow-y-auto"
+            className="bg-base-200 relative"
           >
             {/* Main body (asset browser) */}
             {error && <p>Couldn&apos;t load assets!</p>}
             {isLoading && <p>loading...</p>}
             {assets && (
-              <ul className="grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+              <ul className="absolute inset-0 overflow-y-auto py-4 px-6 grid gap-4 items-start grid-cols-[repeat(auto-fill,minmax(180px,1fr))]">
                 {assets.map(({ id, asset_name, author_pennkey, image_uri }) => (
                   <li key={id}>
                     <button
@@ -48,11 +50,11 @@ function HomeView(): JSX.Element {
                         setSelectedAssetId(id);
                         console.log('set selected id:', id);
                       }}
-                      className={`w-full h-full shadow bg-base-100 p-4 rounded-2xl transition-shadow focus-visible:outline-none ${id === selectedAssetId ? 'ring-2 ring-primary/60 focus-visible:outline-none focus-visible:ring-4' : 'ring-primary/40 focus-visible:ring-4'}`}
+                      className={`w-full h-full shadow bg-base-100 p-4 rounded-2xl transition-shadow focus-visible:outline-none text-left ${id === selectedAssetId ? 'ring-2 ring-primary/60 focus-visible:outline-none focus-visible:ring-4' : 'ring-primary/40 focus-visible:ring-4'}`}
                     >
                       <img
                         src={image_uri || funnygif}
-                        className="aspect-square rounded-lg bg-base-300 mb-2"
+                        className="aspect-square rounded-lg bg-base-300 mb-2 w-full"
                       />
                       <div className="px-1">
                         {asset_name} -- {author_pennkey}
@@ -63,9 +65,11 @@ function HomeView(): JSX.Element {
               </ul>
             )}
           </div>
-          <div className="border-l-[1px] border-base-content/20 px-6 py-4">
-            {/* Asset metadata panel */}
-            {selectedAsset && <Metadata asset={selectedAsset} />}
+          <div className="relative border-l-[1px] border-base-content/20">
+            <div className="absolute inset-0 px-6 py-4">
+              {/* Asset metadata panel */}
+              {selectedAsset && <Metadata asset={selectedAsset} />}
+            </div>
           </div>
         </div>
       </div>
